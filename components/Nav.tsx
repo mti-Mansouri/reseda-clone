e56c5fbf838a60e84bec6101ac57e966e5a86b3e
MehdiMansouri <pioneering.meti@gmail.com>
2025-09-12 01:16:26 +0200

    up to services layout

---
+++ components/Nav.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Nav(){
+
+
+
+    return(
+              <nav>
+        <div className="logo"><a href="">RESEDA PHOTOGRAPHY</a></div>
+
+        <ul>
+          <li><a href="">services</a></li>
+          <li><a href="">gallary</a></li>
+          <li><a href="">information</a></li>
+        </ul>
+
+        <div className="shopping-card"><a href="">cart</a></div>
+      </nav>
+    )
+}
