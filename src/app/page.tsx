62e930d19f73c0c5b20195b628d999e06055b902
MehdiMansouri <pioneering.meti@gmail.com>
2025-09-12 02:46:33 +0200

    section 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,13 +53,74 @@ export default function Home() {
         <h3>WINTER COLLECTION</h3>
         <section className="btn-paragraph">
           <p>
-Photos are metaphors, making the abstract concrete and mystifying the mundane. Be clear, be confident, and don’t overthink it. The beauty of your story is that it’s going to continue to evolve and your site can evolve with it. Your goal should be to make it feel right for right now. Later will take care of itself. 
+            Photos are metaphors, making the abstract concrete and mystifying
+            the mundane. Be clear, be confident, and don’t overthink it. The
+            beauty of your story is that it’s going to continue to evolve and
+            your site can evolve with it. Your goal should be to make it feel
+            right for right now. Later will take care of itself.
           </p>
           <button>View full gallery</button>
         </section>
       </section>
-      <section className="container g2"></section>
-      <section className="container g3"></section>
+      <section className="container g2">
+        <div className="img-wrapper fall1">
+          <img src="part-4-1.webp" alt="" />
+        </div>
+        <div className="img-wrapper fall2">
+          <img src="part-4-2.webp" alt="" />
+        </div>
+        <div className="img-wrapper fall3">
+          <img src="part-4-3.webp" alt="" />
+        </div>
+        <section className="fall-last-imgs">
+          <div className="img-wrapper fall4">
+            <img src="part-4-4.webp" alt="" />
+          </div>
+          <div className="img-wrapper fall5">
+            <img src="part-4-5.webp" alt="" />
+          </div>
+        </section>
+
+        {/* title */}
+        <h3>FALL COLLECTION</h3>
+        <section className="btn-paragraph">
+          <p>
+            Photos are metaphors, making the abstract concrete and mystifying
+            the mundane. Be clear, be confident, and don’t overthink it. The
+            beauty of your story is that it’s going to continue to evolve and
+            your site can evolve with it. Your goal should be to make it feel
+            right for right now. Later will take care of itself.
+          </p>
+          <button>View full gallery</button>
+        </section>
+      </section>
+      <section className="container g3">
+        <div className="img-wrapper lift1">
+          <img src="part-5-1.webp" alt="" />
+        </div>
+        <div className="img-wrapper lift2">
+          <img src="part-5-2.webp" alt="" />
+        </div>
+        <div className="img-wrapper lift3">
+          <img src="part-5-4.webp" alt="" />
+        </div>
+        <div className="img-wrapper lift4">
+          <img src="part-5-3.webp" alt="" />
+        </div>
+
+        {/* title */}
+        <h3>STILL LIFE COLLECTION</h3>
+        <section className="btn-paragraph">
+          <p>
+            Photos are metaphors, making the abstract concrete and mystifying
+            the mundane. Be clear, be confident, and don’t overthink it. The
+            beauty of your story is that it’s going to continue to evolve and
+            your site can evolve with it. Your goal should be to make it feel
+            right for right now. Later will take care of itself.
+          </p>
+          <button>View full gallery</button>
+        </section>
+      </section>
     </section>
   );
 }
