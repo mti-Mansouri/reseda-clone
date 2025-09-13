c660690998145d22ddf88903c41df71705ea566b
MehdiMansouri <pioneering.meti@gmail.com>
2025-09-13 03:18:06 +0200

    gallery-page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,23 +1,27 @@
+import Link from "next/link";
+export default function Nav() {
+  return (
+    <nav>
+      <div className="logo">
+        <Link href="/">RESEDA PHOTOGRAPHY</Link>
+      </div>
 
-
-export default function Nav(){
-
-
-
-    return(
-              <nav>
-        <div className="logo"><a href="">RESEDA PHOTOGRAPHY</a></div>
-
-        <ul>
-          <li><a href="">services</a></li>
-          <li><a href="">gallary</a></li>
-          <li><a href="">information</a></li>
-          <li> 
-        <div className="shopping-card"><a href="">cart</a></div>
-
-          </li>
-        </ul>
-
-      </nav>
-    )
+      <ul>
+        <li>
+          <a href="">services</a>
+        </li>
+        <li>
+          <Link href="/gallery">gallery</Link>
+        </li>
+        <li>
+          <a href="">information</a>
+        </li>
+        <li>
+          <div className="shopping-card">
+            <a href="">cart</a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
 }
