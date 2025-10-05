ae0a46e8b5cf32850dc536f07b4faf6d2e62f1ae
MehdiMansouri <pioneering.meti@gmail.com>
2025-10-05 15:58:13 +0200

    disable success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,32 +1,32 @@
-import Stripe from "stripe";
+// import Stripe from "stripe";
 
-export default async function SuccessPage({
-  searchParams,
-}: {
-  searchParams: { session_id?: string };
-}) {
-  const sessionId = searchParams.session_id;
-  let session: Stripe.Checkout.Session | null = null;
-  if (sessionId) {
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-    session = await stripe.checkout.sessions.retrieve(sessionId);
-  }
+// export default async function SuccessPage({
+//   searchParams,
+// }: {
+//   searchParams: { session_id?: string };
+// }) {
+//   const sessionId = searchParams.session_id;
+//   let session: Stripe.Checkout.Session | null = null;
+//   if (sessionId) {
+//     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+//     session = await stripe.checkout.sessions.retrieve(sessionId);
+//   }
 
-  return (
-    <section
-      style={{
-        marginTop: "100px",
-        padding: "2rem",
-      }}
-    >
-      <h1>Payment Successful 🎉</h1>
-      {session && (
-        <>
-          <p>Thank you for your order!</p>
-          {/* <p>Session ID: {session.id}</p> */}
-          <p>Amount paid: ${session.amount_total! / 100}</p>
-        </>
-      )}
-    </section>
-  );
-}
+//   return (
+//     <section
+//       style={{
+//         marginTop: "100px",
+//         padding: "2rem",
+//       }}
+//     >
+//       <h1>Payment Successful 🎉</h1>
+//       {session && (
+//         <>
+//           <p>Thank you for your order!</p>
+//           {/* <p>Session ID: {session.id}</p> */}
+//           <p>Amount paid: ${session.amount_total! / 100}</p>
+//         </>
+//       )}
+//     </section>
+//   );
+// }
